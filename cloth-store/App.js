29ce512f2d1b956419fa88a97ac59d1145d72db3
kamisettylikhita <kamisettylikitha1234@gmail.com>
2025-08-19 11:19:35 +0530

    Add files via upload

---
+++ cloth-store/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <h1 className='store-title'>My Cloth Store</h1>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
