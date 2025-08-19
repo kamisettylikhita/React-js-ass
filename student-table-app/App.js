3a5489840a792ec3b54e48d3d52b49debb2cb4cc
kamisettylikhita <kamisettylikitha1234@gmail.com>
2025-08-19 11:22:28 +0530

    Add files via upload

---
+++ student-table-app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Studenttable from "./components/Studenttable";
+
+function App() {
+  return (
+    <div>
+      <Studenttable />
+    </div>
+  );
+}
+
+export default App;
