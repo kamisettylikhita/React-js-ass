0b2bf84823e7952310264bb63fdf67890e5dc324
kamisettylikhita <kamisettylikitha1234@gmail.com>
2025-08-19 11:24:46 +0530

    Add files via upload

---
+++ my-nursery-app/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import First from "./components/First";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <First />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
